8fd258888fe6bf3b2a9723e493fae0af34bc28f6
hyoo74 <78240800+hyoo74@users.noreply.github.com>
2021-10-04 19:28:05 -0400

    Update robot_motors.ino

--- robot/robot_motors.ino
+++ robot/robot_motors.ino
@@ -1,1 +1,38 @@
+#include <Servo.h>
+
+Servo my_servo;
+
+uint32_t next;
+
+void setup()
+{
+  // 1000 & 2000 set min/max pulse width in microseconds 
+
+  my_servo.attach(9, 1000, 2000);
+
+  next = millis() + 500;
+}
+
+void loop()
+{
+  static bool rising = true;
+
+  if(millis() > next)
+  {
+    if(rising)
+    {
+      my_servo.write(180);
+      rising = false;
+    }
+    else
+    {
+      my_servo.write(0);
+      rising = true;
+    }
+
+    // repeat again in 3 seconds.
+    next += 3000;
+  }
+
+}
 
